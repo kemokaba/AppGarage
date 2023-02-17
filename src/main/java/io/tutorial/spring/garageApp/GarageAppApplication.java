39b3ab99f4963ac9f5dfe54c6e3790b73da7af17
kemokaba <kemokaba@gmail.com>
2023-02-17 17:06:51 +0100

    first commit

---
+++ src/main/java/io/tutorial/spring/garageApp/GarageAppApplication.java
@@ -0,0 +1,13 @@
+package io.tutorial.spring.garageApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageAppApplication.class, args);
+	}
+
+}
