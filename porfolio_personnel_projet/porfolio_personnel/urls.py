b581c7b1a6f1ea65c763d176855353943332ead4
kemokaba <kemokaba@gmail.com>
2023-02-18 13:40:20 +0100

    certificats

--- porfolio_personnel_projet/porfolio_personnel/urls.py
+++ porfolio_personnel_projet/porfolio_personnel/urls.py
@@ -1,7 +1,7 @@
-"""porfolio_personnel URL Configuration
+"""personal_portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -10,7 +10,7 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import to include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
@@ -19,10 +19,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
+    path('certificat', include('certificat.urls')),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
